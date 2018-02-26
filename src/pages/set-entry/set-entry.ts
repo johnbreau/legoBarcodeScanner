d9ae725ec873cb9dee75f2707e201d46aad3ca89
John Breau <john.breau@vml.com>
2018-02-26 12:28:29 -0600

    added platforms

--- src/pages/set-entry/set-entry.ts
+++ src/pages/set-entry/set-entry.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import { Set } from '../setInterface';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -13,9 +15,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'set-entry',
   templateUrl: 'set-entry.html',
 })
-export class SetEntryPage {
+export class SetEntryPage implements OnInit, OnChanges {
+  public setForm: FormGroup;
+  public DBOpenRequest: any;
+  public db: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.setForm = this.formBuilder.group({
+      setName: [
+        '',
+        Validators.compose([Validators.required])
+      ],
+      setNumber: [
+        '',
+        Validators.compose([Validators.required])
+      ],
+      setPieces: '',
+      setYear: '',
+      setTheme: '',
+      storageLocation: '',
+      disabled: [false]
+    });
+    // Open indexDB Database...
+    this.DBOpenRequest = window.indexedDB.open('setDb', 1);
+
+    this.DBOpenRequest.onerror = function(event) {
+      console.log('Loading...');
+    };
+
+    this.DBOpenRequest.onsuccess = function(event) {
+      console.log('Database initialised');
+    };
+  }
+
+  ngOnChanges() {
+    this.db = this.DBOpenRequest.result;
+
+    const objectStore = this.db.createObjectStore('setDatabase', { keyPath: 'setName' });
+          objectStore.createIndex('setName', 'setName', { unique: false });
+          objectStore.createIndex('setNnumber', 'setNnumber', { unique: true });
+          objectStore.createIndex('setPieces', 'setPieces', { unique: false });
+          objectStore.createIndex('setYear', 'setYear', { unique: false });
+          objectStore.createIndex('setTheme', 'setTheme', { unique: false });
+          objectStore.createIndex('storageLocation', 'storageLocation', { unique: false });
+       console.log('anything?', objectStore);
+  }
+
+  getSet() {
+    window.alert('getSet!');
   }
 
   ionViewDidLoad() {
---
+++ src/pages/setInterface.ts
@@ -0,0 +1,11 @@
+export class Set {
+
+      constructor(
+        public setName: string,
+        public setNumber: string,
+        public setPieces?: number,
+        public setYear?: string,
+        public setTheme?: string
+      ) {  }
+
+}
