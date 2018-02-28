9b586b688c85a3510b261f304868c92ceba11b4a
John Breau <john.breau@vml.com>
2018-02-28 13:55:42 -0600

    getCollection and addSet functions working with stitch

--- src/providers/database-gateway/database-gateway.ts
+++ src/providers/database-gateway/database-gateway.ts
@@ -14,7 +14,6 @@ export class DatabaseGateway{
   constructor(public http: HttpClient) {
   }
 
-
   getCollection() { 
     this.appId = 'legobarcodescanner-eeybg';
     console.log('get set!')
@@ -62,15 +61,6 @@ export class DatabaseGateway{
     });
   }
 
-  // addSet() {
-  //   let headers = new Headers();
-  //   headers.append('Content-Type', 'application/json'); 
-  //   this.http.post('http://localhost:8080/api/reviews', JSON.stringify(set))
-  //     .subscribe(res => {
-  //       console.log(res);
-  //     });
-  // }
-
   findSet(set) {
     this.appId = 'legobarcodescanner-eeybg';
     const clientPromise = StitchClientFactory.create(this.appId);
@@ -96,85 +86,3 @@ export class DatabaseGateway{
   }
  
 }
-
-// getSet() { 
-//   this.appId = 'legobarcodescanner-eeybg';
-//   console.log('get set!')
-//   const clientPromise = StitchClientFactory.create(this.appId);
-  
-//   clientPromise.then(client => {
-//     const db = client.service('mongodb', 'mongodb-atlas').db('legoSetDb');
-//     const sets = db.collection('sets');
-//     client.login().then(()=>
-//       db.collection('sets').find({owner_id: client.authedId()}).limit(100).execute()
-//     ).then(docs => {
-//       console.log("Found docs", docs)
-//       console.log("[MongoDB Stitch] Connected to Stitch")
-//     }).catch(err => {
-//       console.error(err)
-//     });
-//   });
-// }
-
-
-// addSet() { 
-//   this.appId = 'legobarcodescanner-eeybg';
-//   console.log('get set!')
-//   const clientPromise = StitchClientFactory.create(this.appId);
-  
-//   clientPromise.then(client => {
-//     const db = client.service('mongodb', 'mongodb-atlas').db('legoSetDb');
-//     const sets = db.collection('sets');
-//     client.login().then(() =>
-//       db.collection('sets').updateOne(
-//         {setName: 'Galaxy Explorer',
-//          setNumber: '908',
-//          setPieces: 400,
-//          setYear: '1979',
-//          setTheme: 'Space',
-//          setLocation: 'Bin G',
-//          barcodeValue: '123789982399130' })
-//       )
-//       .then(()=>
-//         db.collection('sets').find({owner_id: client.authedId()}).limit(100).execute()
-//       )
-//       .then(docs => {
-//         console.log("Found docs", docs)
-//         console.log("[MongoDB Stitch] Connected to Stitch")
-//       }).catch(err => {
-//         console.error(err)
-//     });
-//   });
-// }
-
-// getSet() { 
-//   this.appId = 'legobarcodescanner-eeybg';
-//   console.log('get set!')
-//   const clientPromise = StitchClientFactory.create(this.appId);
-  
-//   clientPromise.then(client => {
-//     const db = client.service('mongodb', 'mongodb-atlas').db('legoSetDb');
-//     const sets = db.collection('sets');
-//     client.login().then(() =>
-//       // db.collection('sets').updateOne({owner_id: client.authedId()}, {$set:{number:42}}, {upsert:true})
-//       db.collection('sets').insertOne(
-//        {setName: 'Galaxy Explorer',
-//         owner_id: client.authedId(),
-//         setNumber: '908',
-//         setPieces: 400,
-//         setYear: '1979',
-//         setTheme: 'Space',
-//         setLocation: 'Bin G',
-//         barcodeValue: '123789982399130' }
-//       )
-//     ).then(()=>
-//       db.collection('sets')
-//       .then(docs => {
-//       console.log("Found docs", docs)
-//       console.log("[MongoDB Stitch] Connected to Stitch")
-//     }).catch(err => {
-//       console.error(err)
-//     })
-//   )
-// })
-// }
